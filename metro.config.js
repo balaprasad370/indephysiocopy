a20d290cabf0649b53f5ae5687d01e9d84ebe773
Harmandeep Singh <harmandeepsingh@Harmandeeps-MacBook-Air.local>
2024-08-05 00:46:22 +0530

    working jitsi

--- metro.config.js
+++ metro.config.js
@@ -10,26 +10,48 @@
 
 // module.exports = mergeConfig(getDefaultConfig(__dirname), config);
 
-const {getDefaultConfig} = require('metro-config');
+// const {getDefaultConfig} = require('metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig();
+// module.exports = (async () => {
+//   const {
+//     resolver: {sourceExts, assetExts},
+//   } = await getDefaultConfig();
 
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+//   return {
+//     transformer: {
+//       babelTransformerPath: require.resolve('react-native-svg-transformer'),
+//       getTransformOptions: async () => ({
+//         transform: {
+//           experimentalImportSupport: false,
+//           inlineRequires: true,
+//         },
+//       }),
+//     },
+//     resolver: {
+//       assetExts: assetExts.filter(ext => ext !== 'svg'),
+//       sourceExts: [...sourceExts, 'svg'],
+//     },
+//   };
+// })();
+
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const customConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, customConfig);
