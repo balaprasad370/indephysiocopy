8ef4c5b26333fa9dbb7019337a654ac36fd9cb77
Harmandeep Singh <harmandeepsingh@Harmandeeps-MacBook-Air.local>
2024-08-05 00:27:17 +0530

    jitsi-done

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
-};
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Home from './components/Home';
+import Meeting from './components/Meeting';
+
+const RootStack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Screen
+        component={Home}
+        name="Home"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <RootStack.Screen
+        component={Meeting}
+        name="Meeting"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </RootStack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
-
-const styles = StyleSheet.create({});
